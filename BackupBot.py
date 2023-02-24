cf02010432522d5a4ec54488053e3b11f34c4bc8
Dak4rPrO <4382@holbertonschool.com>
2023-02-23 22:42:36 -0300

    Bot por fin Finalizazdo

--- BackupBot.py
+++ BackupBot.py
@@ -1,67 +1,163 @@
-from telethon import TelegramClient, events
 import apiData
 from datetime import datetime
+from telethon import TelegramClient, events
+from pymongo import MongoClient
+
+# CONEXION A MONGODB ATLAS
+clientMongodb = MongoClient('mongodb+srv://MauriMiranda:perracamila24@\
+							firstchatbot-heynow.6bdrqe9.mongodb.net/test')
+db = clientMongodb['Respaldo-de-datos']
+
+# CONEXION AL BOT DE TELEGRAM
+clientTelegram = TelegramClient('session', apiData.api_id, apiData.api_hash)
 
 
-def connect_mongodb():
-    from pymongo import MongoClient
-    """ Function to connect to MongoDB Atlas """
+@clientTelegram.on(events.NewMessage(chats=apiData.chatName))
+async def my_bot(event):
+	
+	user_id = event.sender_id
+	text = event.text.strip()
+	
+	# PRINT PARA DEBUGUEAR EN CONSOLA
+	print(event.text)
 
-    client = MongoClient('mongodb+srv://MauriMiranda:perracamila24@\
-                         firstchatbot-heynow.6bdrqe9.mongodb.net/test')
-    db = client['Respaldo-de-datos']
-    coleccion = db['Plataformas']
-    return coleccion
+	# MANEJO DE COMANDOS
+	if text.startswith('/'):
+		# COMANDO DE INICIO
+		if text == '/start':
+
+			start = "Hola, bienvenido al bot de prueba. Escribe /help para ver \
+los comandos disponibles."
+
+			await clientTelegram.send_message(apiData.chatName, f"{start}")
+
+		# COMANDO DE AYUDA
+		elif text == '/help':
+
+			help_text = "Comandos disponibles:\n"
+
+			help_start = "\n/start: Inicia el bot y crea una nueva colección en \
+la base de datos.\n"
+
+			help_insertar = "\n/insertar <coleccion>: Crea la colección indicada.\
+						   \n/insertar <coleccion>, <username> <contraseña>: \
+De esta manera creara una coleccion (aunque no esta \
+creada) e insertara un usuario y contraseña en \
+ella.\n"
+
+			help = "\n/help: Muestra los comandos disponibles."
+
+			help_listar = "\n/listar: Muestra las colecciones existentes en la \
+base de datos.\n"
+
+			help_mostrar = "\n/mostrar <coleccion>: Muestra todos los usuarios y \
+contraseñas almacenados en la colección indicada.\n"
+
+			help_borrar = "\n/borrar <coleccion>: Borra la colección indicada\
+. La colección debe estar vacía.\n"
+
+			help_quitar = "\n/quitar <coleccion>, <usuario>: Indicando el \
+usuario de una coleccion, borrara todos sus datos.\n"
+
+			await clientTelegram.send_message(apiData.chatName, f"{help_text}\
+				{help_start}{help_insertar}{help_listar}{help_mostrar}{help_borrar}{help_quitar}{help}")
+
+		# LISTAR TODAS LAS COLECCIONES
+		elif text == '/listar':
+			# Obtener una lista de todas las colecciones en la base de datos.
+			collections = db.list_collection_names()
+			# Crear un mensaje que contenga la lista de colecciones.
+			message = "Colecciones en la base de datos:\n\n"
+			message += "\n".join(collections)
+			# Enviar el mensaje al chat de Telegram.
+			await clientTelegram.send_message(apiData.chatName, message)
+		
+		# MOSTRAR DATOS DE COLECCIONES
+		elif text.startswith('/mostrar '):
+			coleccion_name = text[9:].strip()
+			coleccion = db.get_collection(coleccion_name)
+
+			if coleccion_name not in db.list_collection_names():
+				await clientTelegram.send_message(apiData.chatName, f'La colección "{coleccion_name}" no existe.')
+			else:
+				# Obtener todos los documentos de la colección.
+				documentos = coleccion.find()
+				# Crear un mensaje que contenga la lista de usuarios y contraseñas.
+				message = f"Usuarios y contraseñas en la colección {coleccion_name}:\n\n"
+				for documento in documentos:
+					message += f"User: {documento['usuario']} | Clave: {documento['contraseña']}\n"
+				# Enviar el mensaje al chat de Telegram.
+				await clientTelegram.send_message(apiData.chatName, message)
+
+		# GUARDAR COLECCION E INSERTAR DATOS EN COLECCION
+		elif text.startswith('/insertar '):
+			command_parts = text[10:].split(',', 1)
+			coleccion_name = command_parts[0]
+			coleccion = db.get_collection(coleccion_name)
+
+			if len(command_parts) == 1:
+				# Si el comando solo contiene el nombre de la colección,
+				# verifica si la colección existe y si no, crea la colección.
+				if coleccion_name in db.list_collection_names():
+					await clientTelegram.send_message(apiData.chatName, f"La colección {coleccion_name} ya existe. Para insertar datos, utiliza el siguiente formato: /insertar {coleccion_name}, usuario, contraseña")
+				else:
+					db.create_collection(coleccion_name)
+					await clientTelegram.send_message(apiData.chatName, f"La colección {coleccion_name} no existía y ha sido creada. Para insertar datos, utiliza el siguiente formato: /insertar {coleccion_name}, usuario, contraseña")
+			else:
+				# Insertamos los datos en la colección.
+				if coleccion:
+					usuario, contraseña = command_parts[1].strip().split(' ')
+					data = {
+						'user_id': user_id,
+						'usuario': usuario,
+						'contraseña': contraseña,
+						'timestamp': datetime.now(),
+						}
+					coleccion.insert_one(data)
+					await clientTelegram.send_message(apiData.chatName, f"Usuario y contraseña insertados en la colección {coleccion_name}.")
+
+		# BORRAR COLECCION
+		elif text.startswith('/borrar '):
+			coleccion_name = text[8:].strip()
+			coleccion = db.get_collection(coleccion_name)
+
+			if coleccion_name not in db.list_collection_names():
+				await clientTelegram.send_message(apiData.chatName, f'La colección "{coleccion_name}" no existe.')
+			else:
+				# Verificar si la colección está vacía.
+				if coleccion.estimated_document_count() == 0:
+					# Borrar la colección.
+					db.drop_collection(coleccion_name)
+					await clientTelegram.send_message(apiData.chatName, f'La colección "{coleccion_name}" ha sido borrada.')
+				else:
+					await clientTelegram.send_message(apiData.chatName, f'La colección "{coleccion_name}" no está vacía y no puede ser borrada.')
+
+		# QUITAR DATOS DE UNA COLECCION
+		elif text.startswith('/quitar '):
+			command_parts = text[8:].split(',', 1)
+			coleccion_name = command_parts[0]
+			coleccion = db.get_collection(coleccion_name)
+
+			if coleccion_name not in db.list_collection_names():
+				await clientTelegram.send_message(apiData.chatName, f'La colección "{coleccion_name}" no existe.')
+			else:
+				if coleccion.count_documents({}) == 0:
+					await clientTelegram.send_message(apiData.chatName, f'La colección "{coleccion_name}" está vacía.')
+				elif len(command_parts) != 2:
+					await clientTelegram.send_message(apiData.chatName, f'Para quitar datos de la colección "{coleccion_name}", utiliza el siguiente formato: /quitar {coleccion_name}, usuario')
+				else:
+					usuario = command_parts[1].strip()
+					data = coleccion.find_one({'usuario': usuario})
+					if data is None:
+						await clientTelegram.send_message(apiData.chatName, f'El usuario "{usuario}" no existe en la colección "{coleccion_name}".')
+					else:
+						coleccion.delete_one({'usuario': usuario})
+						await clientTelegram.send_message(apiData.chatName, f"Usuario y contraseña borrados de la colección {coleccion_name}.")
+
+		# COMANDO NO VALIDO
+		else:
+			await clientTelegram.send_message(apiData.chatName, "Comando no válido. Escribe /help para ver los comandos disponibles y la sintaxis correcta.")
 
 
-client = TelegramClient('session', apiData.api_id, apiData.api_hash)
-
-@client.on(events.NewMessage(chats=apiData.chatName))
-
-async def my_event_handler(event):
-  
-  data = {
-    'user_id': event.sender_id,
-    'text': event.text,
-    }
-
-  print(event.text)
-  respuesta = event.text
-
-  if not respuesta:
-    await client.send_message(apiData.chatName, "No se pudo procesar su solicitud.")
-    
-  coleccion = connect_mongodb()
-  result = coleccion.insert_one(data)
-  if result.inserted_id:
-    print("Mensaje guardado en la base de datos")
-  else:
-    print("No se pudo guardar el mensaje en la base de datos")
-  
-  if respuesta == '/start':
-    await client.send_message(apiData.chatName, "Hola, bienvenido al bot de prueba.\
-    \n\nEscribe /help para ver los comandos disponibles.")
-  if respuesta == '/help':
-    await client.send_message(apiData.chatName, "Comandos disponibles:\
-    \n\n/start: Inicia el bot\
-    \n/help: Muestra los comandos disponibles\
-    \n/guardar: Guardar los datos de usuario y contraseña\
-    \n/mirar: Ver los datos guardados\
-    \n/borrar: Borrar los datos guardados")
-    
-  plataforma = event.text
-  coleccion = connect_mongodb()
-  
-  coleccion.insert_one({
-        'user_id': event.sender_id,
-        'plataforma': {
-            plataforma: {
-                'usuario': event.text
-            }
-        }
-    })
-
-
-client.start()
-client.run_until_disconnected()
-
+clientTelegram.start()
+clientTelegram.run_until_disconnected()
--- README.md
+++ README.md
@@ -1,21 +1,5 @@
 # Bot_HeyNow  
-  
-  
-Segundo dia y medio del proyecto/desafio ChatBot (En mi caso, bot para preguntar el clima).  
-  
-(1) Rasa instalado, pero no del todo configurado.  
-(2) Tengo funcionando Rasa con la base de datos de MongoDB Atlas (todos mis datos estaran respaldados en la nube).  
-  
-- Con este comando iniciaré el servidor de Rasa y habilitaré la API de Rasa para que pueda interactuar con el bot a través de solicitudes HTTP (Aun debo seguir invesatigando como enviarle las solicitudes)  
-rasa run --enable-api --cors "*" --debug  
-  
-- Intentare fucionar todas las tecnologias para usar la interfaz de telegram  
-  
--------------------------------------------------------------------------------------+  
-RESET   
-  
-NUEVO PROYECTO RECICLANDO CODIGO DEL BOT ANTERIOR  
-  
+
 INICIO DOMINGO 22:00 HS  
   
 bot-interfaz telegram-base de datos de MongoDB Atlas-contraseñas y usuarios-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
